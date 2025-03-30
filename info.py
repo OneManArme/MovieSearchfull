dd59d01fd2aafe011b8510e3a9af0d5331e50bde
OneManArme <asuran1manarmy@gmail.com>
2025-03-30 15:17:11 +0530

    Update info.py

--- info.py
+++ info.py
@@ -45,7 +45,7 @@ SUPPORT_CHAT_ID = int(support_chat_id) if support_chat_id and id_pattern.search(
 
 DATABASE_URI = environ.get('DATABASE_URI', "mongodb+srv://video:video@cluster0.gp0rn.mongodb.net/?retryWrites=true&w=majority")
 DATABASE_NAME = environ.get('DATABASE_NAME', "asuranj")
-COLLECTION_NAME = environ.get('COLLECTION_NAME', 'AutofilterBot')
+COLLECTION_NAME = environ.get('COLLECTION_NAME', 'AutofilterBot1')
 
 # If MULTIPLE_DB Is True Then Fill DATABASE_URI2 Value Else You Will Get Error.
 MULTIPLE_DB = is_enabled(os.environ.get('MULTIPLE_DB', "True"), False) # Type True For Turn On MULTIPLE DB FUNTION 
