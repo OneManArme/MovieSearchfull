8b88acb9b7cf05ac7a0ecef25097f0322ecb64fa
OneManArme <asuran1manarmy@gmail.com>
2025-03-31 22:20:21 +0530

    Update info.py

--- info.py
+++ info.py
@@ -44,7 +44,7 @@ support_chat_id = environ.get('SUPPORT_CHAT_ID', '-1002308503592')
 SUPPORT_CHAT_ID = int(support_chat_id) if support_chat_id and id_pattern.search(support_chat_id) else None
 
 DATABASE_URI = environ.get('DATABASE_URI', "mongodb+srv://Erichdaniken:Erichdaniken@cluster0.vhu3d.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
-DATABASE_NAME = environ.get('DATABASE_NAME', "asuranj")
+DATABASE_NAME = environ.get('DATABASE_NAME', "myserialbot")
 COLLECTION_NAME = environ.get('COLLECTION_NAME', 'AutofilterBot1')
 
 # If MULTIPLE_DB Is True Then Fill DATABASE_URI2 Value Else You Will Get Error.
