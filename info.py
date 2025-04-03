54c86057eec10344c8a5c41cca7e1ef5606bcc27
OneManArme <asuran1manarmy@gmail.com>
2025-04-03 18:19:00 +0530

    Update info.py

--- info.py
+++ info.py
@@ -48,8 +48,8 @@ DATABASE_NAME = environ.get('DATABASE_NAME', "myserialbot")
 COLLECTION_NAME = environ.get('COLLECTION_NAME', 'AutofilterBot1')
 
 # If MULTIPLE_DB Is True Then Fill DATABASE_URI2 Value Else You Will Get Error.
-MULTIPLE_DB = is_enabled(os.environ.get('MULTIPLE_DB', "No"), False) # Type True For Turn On MULTIPLE DB FUNTION 
-DATABASE_URI2 = environ.get('DATABASE_URI2', "")
+MULTIPLE_DB = is_enabled(os.environ.get('MULTIPLE_DB', "True"), False) # Type True For Turn On MULTIPLE DB FUNTION 
+DATABASE_URI2 = environ.get('DATABASE_URI2', "mongodb+srv://asuran1:asuran1@cluster0.cyea86p.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
 
    
 MOVIE_UPDATE_NOTIFICATION = bool(environ.get('MOVIE_UPDATE_NOTIFICATION', False))  
