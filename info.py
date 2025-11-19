0ed6e65993fc2c90b1a77f8d100a7aa28b7e99e0
OneManArme <asuran1manarmy@gmail.com>
2025-11-20 00:38:59 +0530

    Update info.py

--- info.py
+++ info.py
@@ -72,9 +72,9 @@ LOG_VR_CHANNEL = int(environ.get('LOG_VR_CHANNEL', '-1001966055101'))
 LOG_API_CHANNEL = int(environ.get('LOG_API_CHANNEL', '-100'))
 VERIFY_IMG = environ.get("VERIFY_IMG", "https://telegra.ph/file/9ecc5d6e4df5b83424896.jpg")
 
-TUTORIAL = environ.get("TUTORIAL", "http://t.me/demoshort/")
-TUTORIAL_2 = environ.get("TUTORIAL_2", "http://t.me/demoshort/")
-TUTORIAL_3 = environ.get("TUTORIAL_3", "http://t.me/demoshort/")
+TUTORIAL = environ.get("TUTORIAL", "https://t.me/demoshort/")
+TUTORIAL_2 = environ.get("TUTORIAL_2", "https://t.me/demoshort/")
+TUTORIAL_3 = environ.get("TUTORIAL_3", "https://t.me/demoshort/")
 
 SHORTENER_API2 = environ.get("SHORTENER_API2", "3427abe5fbf7b8484de40e64204931163733b73a")
 SHORTENER_WEBSITE2 = environ.get("SHORTENER_WEBSITE2", "babylinks.in")
